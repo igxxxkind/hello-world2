9564a2b9e0d3c803d36f98573ebad360044c7c82
Igor.K <Igor.Kindop@gmail.com>
2021-03-20 14:23:42 +0100

    Igor.K

---
+++ function.r
@@ -0,0 +1,6 @@
+#Function that plots a histogram with some standardized outputs
+function = printhist(data){
+  hist(data,probability = T)
+  abline(v = mean(data), lwd = 2, lty = 2)
+  abline(v = c(-1,1)*sd(data), lwd = 1, col = "red")
+}
---
+++ git.course.r
@@ -0,0 +1,23 @@
+##
+print("Hello Git!")
+set.seed(12345)
+#check if the LL CLT works
+
+#create a matrix of random variables with Poisson(5) distribution
+RNmat = matrix(rpois(10000,lambda = 5),100,100)
+
+#consider columnwise sums
+colsum = apply(RNmat,2,sum)
+
+#the vector should be Normally distributed with mean nmu and standard deviation nsigma^2
+
+#check for normality
+jarque.bera.test(colsum)
+
+source("function.r")
+
+
+
+printhist(colsum)
+
+

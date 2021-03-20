87fe7830cc1e85b7daed32aea10abf572b1b2633
Igor.K <Igor.Kindop@gmail.com>
2021-03-20 14:50:00 +0100

    add function to the body

--- git.course.r
+++ git.course.r
@@ -16,10 +16,14 @@ jarque.bera.test(colsum)
 
 Box.test(x = colsum,lag = 8,type = c("Ljung-Box"))
 
-source("function.r")
-
-
 
 printhist(colsum)
+
+#Function that plots a histogram with some standardized outputs
+printhist = function(data){
+  hist(data,probability = T)
+  abline(v = mean(data), lwd = 2, lty = 2)
+  abline(v = c(-1,1)*sd(data), lwd = 1, col = "red")
+}
 
 
